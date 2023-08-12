a3a3667e53194b181d03ba1e5f1cb776745ba0d7
dogukaan1 <dgkaan67@gmail.com>
2023-08-12 15:38:54 +0300

    sehir tahmin oyunu
    javascript dilinde yazdığım seçtiğiniz bölgeden rastgele şehir seçerek size bunlar hakkında bilgi veren ve bu şehri tahmin etmenizi isteyen bir proje

---
+++ sehir_tahmin/script.js
@@ -0,0 +1,9 @@
+let [[marmaraSehir,karadenizSehir,akdenizSehir,egeSehir,doguAnadoluSehir,guneyDoguAnadoluSehir,icAnadoluSehir]]=[[
+    [ "Çanakkale", "Balıkesir", "Edirne", "Tekirdağ", "Kırklareli", "İstanbul", "Bursa", "Yalova", "Kocaeli", "Bilecik" , "Sakarya" ],
+    ["Bolu", "Düzce", "Zonguldak", "Karabük", "Bartın", "Kastamonu", "Çorum", "Sinop", "Samsun", "Amasya", "Tokat", "Ordu", "Giresun", "Gümüşhane", "Trabzon", "Bayburt", "Rize" , "Artvin"],
+    ["Antalya", "Burdur", "Isparta", "Mersin"," Adana", "Hatay", "Osmaniye ","Kahramanmaraş"],
+    ["İzmir", "Aydın", "Muğla", "Manisa", "Denizli", "Uşak", "Kütahya" ,"Afyon"],
+    ["Malatya", "Erzincan", "Elazığ", "Tunceli", "Bingöl", "Erzurum", "Muş", "Bitlis", "Şırnak", "Kars", "Ağrı", "Ardahan", "Van", "Iğdır" , "Hakkari"],
+    [ "Gaziantep", "Kilis", "Adıyaman", "Şanlıurfa", "Diyarbakır", "Mardin", "Batman" , "Siirt"],
+    ["Eskişehir"," Konya", "Ankara", "Çankırı", "Aksaray", "Kırıkkale", "Kırşehir", "Yozgat", "Niğde", "Nevşehir"," Kayseri", "Karaman" ,"Sivas"]]];
+  document.write(marmaraSehir[3]);
